7a3cb9a8d9d9d1beccb3c00184d8bbcfdef064bb
liuxiaobai666 <liuxiaobaia@163.com>
2024-10-08 11:41:01 +0800

    01_分析工程目录结构

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
+// 引入createApp工厂函数
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// 创建应用实例对象-app 
+const app = createApp(App)
+
+// 挂载app
+app.mount('#app')
+// createApp(App).mount('#app')
