593fe87736c214b3d808cb2a904d8812ba754798
liuxiaobai666 <liuxiaobaia@163.com>
2024-10-15 15:15:25 +0800

    18_provide和inject

---
+++ 18_provide和inject/components/Son.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="son">
+    <h3>我是Son组件(孙),{{ car.name }} -- {{ car.price }}</h3>
+    <button @click="car.name += '!'">修改car.name</button>
+  </div>
+</template>
+
+<script>
+import { inject } from 'vue';
+
+export default {
+  name:'Son',
+  setup(){
+    let car = inject('car')
+    console.log(car);
+    return {car}
+    
+  }
+}
+</script>
+
+<style scoped>
+.son{
+  background-color: orange;
+  padding: 10px;
+}
+</style>
